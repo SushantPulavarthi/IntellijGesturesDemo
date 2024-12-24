b7e2e828e209f5c961649ff6fd0b2e4d17748537
Sushant Pulavarthi <sushant.pulavarthi09@gmail.com>
2024-12-24 23:03:08 +0000

    Impl: Listeners to move image based on mouse movement

--- src/main/kotlin/com/sushant/intellijgesturedemo/GestureWindow.kt
+++ src/main/kotlin/com/sushant/intellijgesturedemo/GestureWindow.kt
@@ -4,6 +4,9 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import java.awt.Dimension
 import java.awt.Point
+import java.awt.Toolkit
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.ImageIcon
 import javax.swing.JFrame
 import javax.swing.JLabel
@@ -26,6 +29,25 @@ class GestureWindow : DumbAwareAction() {
             (frame.height - imageIcon.iconHeight) / 2
         )
         frame.add(label)
+
+        frame.addMouseListener(object : MouseAdapter() {
+            override fun mouseEntered(e: MouseEvent) {
+                label.isVisible = true
+            }
+
+            override fun mouseExited(e: MouseEvent?) {
+                label.isVisible = false
+            }
+        })
+
+        frame.addMouseMotionListener(object : MouseAdapter() {
+            override fun mouseMoved(e: MouseEvent) {
+                label.location = Point(
+                    e.x - imageIcon.iconWidth / 2,
+                    e.y - imageIcon.iconHeight / 2
+                )
+            }
+        })
     }
 
     override fun actionPerformed(e: AnActionEvent) {
