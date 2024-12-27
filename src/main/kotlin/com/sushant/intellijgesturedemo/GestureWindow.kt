4ae27430377eb7a898b86dabca49c6c38edb6217
Sushant Pulavarthi <sushant.pulavarthi09@gmail.com>
2024-12-27 00:15:00 +0000

    Impl: Image scales no matter which side and direction

--- src/main/kotlin/com/sushant/intellijgesturedemo/GestureWindow.kt
+++ src/main/kotlin/com/sushant/intellijgesturedemo/GestureWindow.kt
@@ -4,24 +4,32 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import java.awt.Dimension
 import java.awt.Point
-import java.awt.Toolkit
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.ImageIcon
 import javax.swing.JFrame
 import javax.swing.JLabel
 
-
 class GestureWindow : DumbAwareAction() {
+    companion object {
+        const val WINDOW_WIDTH = 800
+        const val WINDOW_HEIGHT = 600
+        const val MINIMUM_SIZE = 0.25
+        const val MAXIMUM_SIZE = 1.0
+        const val SCALE_FACTOR = 500.0
+    }
+
     private val frame = JFrame("Gesture Window")
+    private val imageIcon = ImageIcon(javaClass.getResource("/jetbrains.png"))
+    private val label = JLabel()
+    private var enteredAt = Point()
+    private var lastPos = Point()
+    private var currentScale = 1.0
 
     init {
-        println("Creating Window")
-        frame.setSize(800, 600)
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT)
         frame.layout = null
 
-        val imageIcon = ImageIcon(javaClass.getResource("/jetbrains.png"))
-        val label = JLabel()
         label.icon = imageIcon
         label.size = Dimension(imageIcon.iconWidth, imageIcon.iconHeight)
         label.location = Point(
@@ -32,6 +40,10 @@ class GestureWindow : DumbAwareAction() {
 
         frame.addMouseListener(object : MouseAdapter() {
             override fun mouseEntered(e: MouseEvent) {
+                enteredAt = e.point
+                lastPos = e.point
+                currentScale = MINIMUM_SIZE
+                scaleAndPositionImage(e.point)
                 label.isVisible = true
             }
 
@@ -42,15 +54,49 @@ class GestureWindow : DumbAwareAction() {
 
         frame.addMouseMotionListener(object : MouseAdapter() {
             override fun mouseMoved(e: MouseEvent) {
-                label.location = Point(
-                    e.x - imageIcon.iconWidth / 2,
-                    e.y - imageIcon.iconHeight / 2
-                )
+                if (!label.isVisible) return
+
+                val deltaChange = e.point.distance(lastPos)
+                if (deltaChange != 0.0) {
+                    println(deltaChange)
+                    val scaleChange = deltaChange / SCALE_FACTOR
+                    // If moves back towards entered out, scale image down
+                    currentScale += if (enteredAt.distance(e.point) < enteredAt.distance(lastPos)) {
+                        -scaleChange
+                    } else {
+                        scaleChange
+                    }
+                    println(currentScale)
+                    currentScale = currentScale.coerceIn(MINIMUM_SIZE, MAXIMUM_SIZE)
+                    scaleAndPositionImage(e.point)
+                }
+
+                lastPos = e.point
             }
         })
     }
 
     override fun actionPerformed(e: AnActionEvent) {
         frame.isVisible = !frame.isVisible
+    }
+
+    fun scaleAndPositionImage(pos: Point) {
+        // Scale Image
+        val imageWidth = imageIcon.iconWidth
+        val imageHeight = imageIcon.iconHeight
+        label.icon = ImageIcon(
+            imageIcon.image.getScaledInstance(
+                (imageWidth * currentScale).toInt(),
+                (imageHeight * currentScale).toInt(),
+                java.awt.Image.SCALE_SMOOTH
+            )
+        )
+        label.size = Dimension(label.icon.iconWidth, label.icon.iconHeight)
+
+        // Put image at centre of cursor
+        label.location = Point(
+            pos.x - label.width / 2,
+            pos.y - label.height / 2
+        )
     }
 }
